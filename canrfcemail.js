e32c16a9b0367cb3745391116676af247cb384f7
chris <chris@bumblehead.com>
2014-05-11 16:35:07 -0700

    changed canrfcemail _isrfcemail_ to _canrfcemail_

--- canrfcemail.js
+++ canrfcemail.js
@@ -1,7 +1,7 @@
 // Filename: canrfcemail.js
-// Timestamp: 2014.04.20-19:04:23 (last modified)  
+// Timestamp: 2014.05.11-16:34:11 (last modified)  
 // Author(s): Bumblehead (www.bumblehead.com)  
 
-var isrfcemail = ((typeof module === 'object') ? module : {}).exports = function (email) {  
+var canrfcemail = ((typeof module === 'object') ? module : {}).exports = function (email) {  
   return /^[^ ].*@.*[^ ]$/.test(email);
 };
